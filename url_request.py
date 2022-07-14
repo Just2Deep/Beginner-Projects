bfba2af58a4980e87b64451f40aa9292f3501deb
Just2Deep <deepakrajsrs@gmail.com>
2022-07-14 16:05:35 +0530

    reading data from web

---
+++ url_request.py
@@ -0,0 +1,15 @@
+# Scrape Table from a Website using Python
+
+import urllib.request
+import pandas as pd
+url = "https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites"
+
+with urllib.request.urlopen(url) as i:
+    html = i.read()
+
+data = pd.read_html(html)[0]
+print(data.head())
+
+# saving the data to csv
+
+data.to_csv("programming.csv")
